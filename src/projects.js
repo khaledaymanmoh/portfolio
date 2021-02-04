28f896deec5848d4fc9cf59883d6b4f74e602462
Khaled Ayman <khaledaymanmoh@gmail.com>
2021-02-04 14:49:51 +0200

    spell-checks

--- src/projects.js
+++ src/projects.js
@@ -4,14 +4,14 @@ const projects = [
     {
         id: 1,
         title: "Waves Music Player",
-        description: "Developed this thorough out The Creative React Course by Dev Ed, my favorite instructor on the internet. The project is very interesting and included the implementation of a variety of concepts and features which enhanced my journey in using ReactJS. ",
+        description: "Developed this through out The Creative React Course by Dev Ed, my favorite instructor on the internet. The project is very interesting and included the implementation of a variety of concepts and features which enhanced my journey in using ReactJS. ",
         image: reactwavesplayer,
         using: ["ReactJS"], link: "https://react-waves-player.netlify.app/"
     },
     {
         id: 2,
         title: "Todo List",
-        description: "My first by-myself react project, it is extremely basic, however, I was keen on implementing it to put the ReactJS basics I learned into action, props, states, etc. along with some cool libraries such as React Router and Framer Motion.",
+        description: "My first by-myself react project, it is extremely basic. However, I was keen on implementing it to put the ReactJS basics I learned into action, props, states, etc. along with some cool libraries such as React Router and Framer Motion.",
         image: reacttodo,
         using: ["ReactJS"], link: "http://khaloudastodo.netlify.app"
     }
