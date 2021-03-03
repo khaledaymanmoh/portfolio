3915157ffa9919162193f52bd3d8a047185bcc07
Khaled Ayman <khaledaymanmoh@gmail.com>
2021-03-03 15:00:02 +0200

    new -proj

--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,7 @@
 //Project Screenshots
 import reacttodo from "./images/reacttodo.JPG";
 import reactwavesplayer from "./images/reactwavesplayer.JPG";
+import reactgallery from "./images/reactgallery.JPG";
 const projects = [
     {
         id: 1,
@@ -15,6 +16,13 @@ const projects = [
         description: "My first by-myself react project, it is extremely basic. However, I was keen on implementing it to put the ReactJS basics I learned into action, props, states, etc. along with some cool libraries such as React Router and Framer Motion.",
         image: reacttodo,
         using: ["ReactJS"], link: "http://khaloudastodo.netlify.app"
+    },
+    {
+        id: 3,
+        title: "React Gallery",
+        description: "A first step towards back-end development, I chose firebase due its simplicity.. Created an online gallery that saves uploaded photos in a firestore storage and present them in a simple grid, added styles using Styled Components and simple animations using Framer Motion.",
+        image: reactgallery,
+        using: ["ReactJS", "Firebase"], link: "http://fire-reactgram.netlify.app"
     }
 
 ];
